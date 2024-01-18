4767ccc7c5d7aeb9f3c6870d896d8e3d42cab61f
assunnahmuslimit <abdullah.limonbau@gmail.com>
2024-01-18 15:53:39 +0000

    initial commit

--- app/post/page.js
+++ app/post/page.js
@@ -1,14 +1,14 @@
 import "./soon.css";
 export default function BlogPage() {
   return (
-    <div class="bgimg">
-      <div class="topleft">Welcome</div>
-      <div class="middle">
+    <div className="bgimg">
+      <div className="topleft">Welcome</div>
+      <div className="middle">
         <h1>COMING SOON</h1>
         <hr />
         <p>Blogs are under construction</p>
       </div>
-      <div class="bottomleft">
+      <div className="bottomleft">
         <p>Internet Madrasa</p>
       </div>
     </div>
