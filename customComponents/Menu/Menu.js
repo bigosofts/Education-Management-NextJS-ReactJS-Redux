3ef44c8c6be589e46e717cf34d7a403b35fd2490
assunnahmuslimit <abdullah.limonbau@gmail.com>
2024-01-22 18:39:59 +0000

    Final Backened Frontend separation

--- customComponents/Menu/Menu.js
+++ customComponents/Menu/Menu.js
@@ -49,7 +49,7 @@ const MainMenu = () => {
           <div className="style-16">
             <div className="style-17">
               <div className="style-18">
-                <a href="/old-site">
+                <a href="http://assunnahmuslimit.com/registration.php">
                   <button>Registration</button>
                 </a>
               </div>
--- customComponents/newSignupFrame/NewSignupFrame.js
+++ customComponents/newSignupFrame/NewSignupFrame.js
@@ -1,13 +1,9 @@
-import SubMenu from "../SubMenu/SubMenu";
-import MainMenu from "../Menu/Menu";
 function NewSignupFrame() {
   return (
     <>
-      <MainMenu />
-      <SubMenu />
       <iframe
         style={{ position: "unset", width: "100%", height: "100vh" }}
-        src="http://assunnahmuslimit.com/registration.php"
+        src="https://assunnahmuslimit.com/registration.php"
         title="description"
       ></iframe>
     </>
