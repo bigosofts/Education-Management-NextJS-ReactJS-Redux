e987ab947957f5a514004dc2ade4ca92391b6cc2
assunnahmuslimit <abdullah.limonbau@gmail.com>
2024-01-22 10:30:57 +0000

    Final Backened Frontend separation

--- customComponents/newSignupFrame/NewSignupFrame.js
+++ customComponents/newSignupFrame/NewSignupFrame.js
@@ -1,7 +1,10 @@
-"use server";
+import SubMenu from "../SubMenu/SubMenu";
+import MainMenu from "../Menu/Menu";
 function NewSignupFrame() {
   return (
     <>
+      <MainMenu />
+      <SubMenu />
       <iframe
         style={{ position: "unset", width: "100%", height: "100vh" }}
         src="http://assunnahmuslimit.com/registration.php"
