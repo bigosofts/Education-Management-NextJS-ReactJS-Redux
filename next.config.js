0ce5212afb0869633a08a3af79079dea6ad9d307
assunnahmuslimit <abdullah.limonbau@gmail.com>
2024-01-23 06:45:06 +0000

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["api.internetmadrasa.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.internetmadrasa.com",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
   },
 };
 
