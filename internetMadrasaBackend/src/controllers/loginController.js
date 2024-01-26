2a9c3c0edc65ec797dc9a46f758667fec9dc68c9
assunnahmuslimit <abdullah.limonbau@gmail.com>
2024-01-26 12:59:38 +0600

    login page edit

--- internetMadrasaBackend/src/controllers/loginController.js
+++ internetMadrasaBackend/src/controllers/loginController.js
@@ -15,24 +15,15 @@ exports.studentLogin = (req, res) => {
     .then((data) => {
       // Create Auth Token
       let Payload = {
-        exp: Math.floor(Date.now() / 1000) + 720 * 60 * 60,
+        exp: Math.floor(Date.now() / 1000) + 365 * 24 * 60 * 60,
         data: data,
       };
       let token = jwt.sign(Payload, process.env.SECRETKEY_JWT_WEBTOKEN);
-      res
-        .cookie("access_token", token, {
-          httpOnly: true,
-          maxAge: 30 * 24 * 60 * 60 * 1000,
-          sameSite: "None",
-          secure: true,
-          domain: "internetmadrasa.com",
-        })
-        .status(200)
-        .json({
-          status: "Alhamdulillah",
-          token: token,
-          data: data,
-        });
+      res.status(200).json({
+        status: "Alhamdulillah",
+        token: token,
+        data: data,
+      });
     })
     .catch((err) => {
       res.status(400).json({
@@ -53,7 +44,7 @@ exports.teacherLogin = (req, res) => {
     .then((data) => {
       // Create Auth Token
       let Payload = {
-        exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
+        exp: Math.floor(Date.now() / 1000) + 365 * 24 * 60 * 60,
         data: data,
       };
       let token = jwt.sign(Payload, process.env.SECRETKEY_JWT_WEBTOKEN);
