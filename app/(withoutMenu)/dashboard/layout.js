18d0e12bcd455ea1e697dc39567dabb79083405f
assunnahmuslimit <abdullah.limonbau@gmail.com>
2024-06-21 22:44:02 +0600

    Fixed ADmin Dashboard issue on dashboar layout js

--- app/(withoutMenu)/dashboard/layout.js
+++ app/(withoutMenu)/dashboard/layout.js
@@ -9,6 +9,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { setInitialData } from "@/app/redux/features/isAdmin/isAdminSlice";
 
 import { selectDataTwo } from "@/apiservices/studentapiservices";
+import { selectAllDataTwo } from "@/apiservices/teacherapiservices";
 
 function DashboardLayout({ loggedin, notloggedin }) {
   const [data, setData] = useState();
@@ -17,24 +18,44 @@ function DashboardLayout({ loggedin, notloggedin }) {
   useEffect(() => {
     async function fetchData() {
       const payload = await isAdmin();
+
       if (payload.status == "Alhamdulillah") {
-        const res = await selectDataTwo(
-          { userName: payload.data.userName },
-          null
-        );
+        if (payload.data.userRole == "student") {
+          const res = await selectDataTwo(
+            { userName: payload.data.userName },
+            null
+          );
+          if (res.status == "Alhamdulillah") {
+            const desiredObj = {
+              status: "Alhamdulillah",
+              data: {
+                userName: res.data[0].userName,
+                userRole: res.data[0].userRole,
+                isAdmin: res.data[0].isAdmin,
+                userDetails: res.data[0],
+              },
+            };
 
-        if (res.status == "Alhamdulillah") {
-          const desiredObj = {
-            status: "Alhamdulillah",
-            data: {
-              userName: res.data[0].userName,
-              userRole: res.data[0].userRole,
-              isAdmin: res.data[0].isAdmin,
-              userDetails: res.data[0],
-            },
-          };
+            dispatch(setInitialData(desiredObj));
+          }
+        } else if (payload.data.userRole == "teacher") {
+          const res = await selectAllDataTwo(
+            { userName: payload.data.userName },
+            null
+          );
+          if (res.status == "Alhamdulillah") {
+            const desiredObj = {
+              status: "Alhamdulillah",
+              data: {
+                userName: res.data[0].userName,
+                userRole: res.data[0].userRole,
+                isAdmin: res.data[0].isAdmin,
+                userDetails: res.data[0],
+              },
+            };
 
-          dispatch(setInitialData(desiredObj));
+            dispatch(setInitialData(desiredObj));
+          }
         }
       }
       setData(payload);
