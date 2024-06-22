c58e547f6771dc06027dc38cd20e7aceb0384532
assunnahmuslimit <abdullah.limonbau@gmail.com>
2024-06-23 01:04:34 +0600

    Add progressbar in dashboard 3

--- components/dashboardPage/progressBar.js
+++ components/dashboardPage/progressBar.js
@@ -1,13 +1,40 @@
+"use client";
+import { useEffect, useState } from "react";
+
 function ProgressBar({ percentage }) {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    let timeoutId;
+    if (percentage === 100) {
+      timeoutId = setTimeout(() => {
+        setVisible(false);
+      }, 2000); // 10 seconds delay
+    } else {
+      setVisible(true); // Reset visibility if percentage is not 100
+    }
+
+    return () => clearTimeout(timeoutId); // Clear timeout if component unmounts or percentage changes
+  }, [percentage]);
+
+  if (!visible) {
+    return null;
+  }
+
   return (
-    <div className="w-full bg-white rounded-full dark:bg-gray-700 mb-10">
-      <div
-        className="bg-blue-600 text-xl font-xl text-blue-100 text-center p-2 leading-none rounded-full"
-        style={{ width: percentage + "%", transition: "2s ease-out" }}
-      >
-        {percentage > 50 && "Loading your data: "} {percentage}%
+    <>
+      <p className="text-xm text-slate-800 text-center">
+        Loading your data ...
+      </p>
+      <div className="w-full bg-white rounded-full dark:bg-gray-700 mb-10">
+        <div
+          className="bg-blue-600 text-xl font-xl text-blue-100 text-center p-2 leading-none rounded-full"
+          style={{ width: percentage + "%", transition: "2s ease-out" }}
+        >
+          {percentage}%
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
